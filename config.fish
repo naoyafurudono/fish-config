f8874ecdc5de2841a0193f7c947713be664ca7dc
naoyafurudono <naoyafurudono@prg.is.titech.ac.jp>
2022-05-29 23:28:27 +0900

    update

--- config.fish
+++ config.fish
@@ -8,14 +8,17 @@ if status --is-interactive
   abbr --add commit   git commit
   abbr --add push     git push
   abbr --add status   git status
+  abbr --add xremap   xremap ~/.config/xremap/xremap.conf --device 'Topre REALFORCE 87 US' 
 end
 
 function gi; curl -sLw n https://www.toptal.com/developers/gitignore/api/$argv; end
 
-set PATH /Users/nfurudono/.cargo/bin $PATH
 set PATH ~/go/bin $PATH
+set PATH /usr/local/go/bin $PATH
 set PATH ~/.local/bin $PATH
 set PATH ~/.cargo/bin $PATH
 set EDITOR nvim
-set PAGER bat
+set VISUAL nvim
+# set PAGER bat
 
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/furudono/.ghcup/bin $PATH # ghcup-env
